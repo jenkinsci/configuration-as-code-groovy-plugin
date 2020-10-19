316f5bf1622f9a45e027c97ac8ad346fbc461452
Joseph Petersen <josephp90@gmail.com>
2020-10-19 23:28:38 +0200

    fix deprecated method and improve readability

--- src/main/java/io/jenkins/plugins/cascgroovy/GroovyScriptCaller.java
+++ src/main/java/io/jenkins/plugins/cascgroovy/GroovyScriptCaller.java
@@ -3,17 +3,24 @@ package io.jenkins.plugins.cascgroovy;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import hudson.Extension;
-import io.jenkins.plugins.casc.*;
+import io.jenkins.plugins.casc.Attribute;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.Configurator;
+import io.jenkins.plugins.casc.ConfiguratorException;
+import io.jenkins.plugins.casc.RootElementConfigurator;
 import io.jenkins.plugins.casc.impl.attributes.MultivaluedAttribute;
 import io.jenkins.plugins.casc.model.CNode;
 import io.jenkins.plugins.casc.model.Mapping;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import jenkins.model.Jenkins;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
-
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-import java.util.*;
 
 import static io.vavr.API.Try;
 import static io.vavr.API.unchecked;
@@ -27,6 +34,7 @@ import static io.vavr.API.unchecked;
 public class GroovyScriptCaller implements RootElementConfigurator<Boolean[]> {
 
     @Override
+    @Nonnull
     public String getName() {
         return "groovy";
     }
@@ -37,6 +45,7 @@ public class GroovyScriptCaller implements RootElementConfigurator<Boolean[]> {
     }
 
     @Override
+    @Nonnull
     public Set<Attribute<Boolean[],?>> describe() {
         return Collections.singleton(new MultivaluedAttribute("", GroovyScriptSource.class));
     }
@@ -47,13 +56,13 @@ public class GroovyScriptCaller implements RootElementConfigurator<Boolean[]> {
     }
 
     @Override
+    @Nonnull
     public Boolean[] configure(CNode config, ConfigurationContext context) throws ConfiguratorException {
         final Configurator<GroovyScriptSource> c = context.lookup(GroovyScriptSource.class);
         return config.asSequence().stream()
             .map(source -> getActualValue(source, context))
-            .map(source -> Try(() -> c.configure(source, context).getScript())
-                                .onSuccess(GroovyScriptCaller.this::runGroovyShell)
-                                .isSuccess())
+            .map(source -> getScriptFromSource(source, context, c))
+            .map(script -> Try(script::getScript).onSuccess(GroovyScriptCaller.this::runGroovyShell).isSuccess())
             .toArray(Boolean[]::new);
     }
 
@@ -67,13 +76,21 @@ public class GroovyScriptCaller implements RootElementConfigurator<Boolean[]> {
         final Mapping m = new Mapping();
         m.put(
             entry.getKey(),
-            SecretSourceResolver.resolve(context, unchecked(() -> entry.getValue().asScalar().getValue()).apply())
+            context.getSecretSourceResolver().resolve(unchecked(() -> entry.getValue().asScalar().getValue()).apply())
         );
         return m;
     }
 
+    private GroovyScriptSource getScriptFromSource(CNode source, ConfigurationContext context,
+            Configurator<GroovyScriptSource> configurator) {
+        return unchecked(() ->
+                Try(() -> configurator.configure(source, context))
+                        .getOrElseThrow(t -> new ConfiguratorException(this,
+                                "Failed to retrieve groovy script", t))).apply();
+    }
+
     private void runGroovyShell(String script) {
-        final GroovyShell s = new GroovyShell(Jenkins.getActiveInstance().getPluginManager().uberClassLoader, new Binding());
+        final GroovyShell s = new GroovyShell(Jenkins.get().getPluginManager().uberClassLoader, new Binding());
         unchecked(() -> s.run(script, "ConfigurationAsCodeGroovy", new ArrayList()));
     }
 
